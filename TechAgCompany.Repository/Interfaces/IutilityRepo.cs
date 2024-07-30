589e0b22d183f683d5a727facc8783b9e8046157
ashutosh.gourshete <ashutosh.gourshete@promptsoftech.in>
2024-07-30 15:20:30 +0530

    Repository for File Save in Local Storage in ASP.NET CORE

---
+++ TechAgCompany.Repository/Interfaces/IutilityRepo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechAgCompany.Repository.Interfaces
+{
+    public  interface IutilityRepo
+    {
+        Task<string> SaveImage(string ContainerName, IFormFile file);
+        Task<string> EditImage(string ContainerName, IFormFile file,string dbpath);
+
+        Task deleteImage(string ContainerName,string dbpath);
+        
+    }
+}
